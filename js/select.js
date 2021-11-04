5b224fc8bc0ec0e03e3a21ced05f802ffb29c6f6
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-11-04 07:12:50 +0200

    +

--- js/select.js
+++ js/select.js
@@ -20,30 +20,13 @@
 //     });
 //   });
 // });
-jQuery(($) => {
-  $(".select").on("click", ".select__head", function () {
-    if ($(this).hasClass("open")) {
-      $(this).removeClass("open");
-      $(this).next().fadeOut();
-    } else {
-      $(".select__head").removeClass("open");
-      $(".select__list").fadeOut();
-      $(this).addClass("open");
-      $(this).next().fadeIn();
-    }
-  });
 
-  $(".select").on("click", ".select__item", function () {
-    $(".select__head").removeClass("open");
-    $(this).parent().fadeOut();
-    $(this).parent().prev().text($(this).text());
-    $(this).parent().prev().prev().val($(this).text());
-  });
+// let select = function () {
+//   let selectHeader = document.querySelectorAll(".select-header");
 
-  $(document).click(function (e) {
-    if (!$(e.target).closest(".select").length) {
-      $(".select__head").removeClass("open");
-      $(".select__list").fadeOut();
-    }
-  });
-});
+//   selectHeader.forEach((item) => {
+//     item.addEventListener("click", function () {
+//       this.parentElement.classList.togle("is-activ");
+//     });
+//   });
+// };
